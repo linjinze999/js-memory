8478f4cb083ea608f264aed73e13567c716eb871
jinzelin <jinzelin@tencent.com>
2022-08-14 00:19:19 +0800

    feat: init

--- src/v8/snapshot/V8Snapshot.ts
+++ src/v8/snapshot/V8Snapshot.ts
@@ -1,5 +1,5 @@
 import {
-  V8SnapshotInfo,
+  V8SnapshotInfo, V8SnapshotInfoEdge,
   V8SnapshotInfoNode,
   V8SnapshotProgressParams
 } from './V8SnapshotInfo';
@@ -8,6 +8,15 @@ import { V8SnapshotEdgeTypes, V8SnapshotNodeTypes } from './V8SnapshotTypes';
 export interface SnapshotOptions{
   text: string;
   progressCallback?: (params: V8SnapshotProgressParams) => void;
+}
+interface V8SnapshotCalculateSize {
+  total: number, // 总计
+  native: number, // 类型化数组
+  code: number, // 代码
+  string: number, // 字符串
+  array: number, // js数组
+  system: number, // 系统对象
+  others: number // 其他
 }
 
 export class V8Snapshot {
@@ -23,42 +32,46 @@ export class V8Snapshot {
 
   private readonly options: SnapshotOptions = {} as any;
 
+
   public snapshot_info: V8SnapshotInfo;
+  private calculateSize?: V8SnapshotCalculateSize;
 
   // 统计数据
-  public calculateStatistics = () => {
-    const size = {
-      total: 0, // 总计
-      native: 0, // 类型化数组
-      code: 0, // 代码
-      string: 0, // 字符串
-      array: 0, // js数组
-      system: 0, // 系统对象
-      others: 0 // 其他
-    };
-    let nodeSize: number;
-    this.snapshot_info.node_list.forEach((node) => {
-      nodeSize = node.self_size as number;
-      size.total += nodeSize;
-      if (node.distance >= V8SnapshotInfo.BASE_SYSTEM_DISTANCE) {
-        size.system += nodeSize;
-      } else if (node.type === V8SnapshotNodeTypes.native) {
-        size.native += nodeSize;
-      } else if (node.type === V8SnapshotNodeTypes.code) {
-        size.code += nodeSize;
-      } else if (([
+  public calculateStatistics = (reset?: boolean): V8SnapshotCalculateSize => {
+    if (!this.calculateSize || reset) {
+      this.calculateSize = {
+        total: 0, // 总计
+        native: 0, // 类型化数组
+        code: 0, // 代码
+        string: 0, // 字符串
+        array: 0, // js数组
+        system: 0, // 系统对象
+        others: 0 // 其他
+      };
+      let nodeSize: number;
+      this.snapshot_info.node_list.forEach((node) => {
+        nodeSize = node.self_size as number;
+        this.calculateSize.total += nodeSize;
+        if (node.distance >= V8SnapshotInfo.BASE_SYSTEM_DISTANCE) {
+          this.calculateSize.system += nodeSize;
+        } else if (node.type === V8SnapshotNodeTypes.native) {
+          this.calculateSize.native += nodeSize;
+        } else if (node.type === V8SnapshotNodeTypes.code) {
+          this.calculateSize.code += nodeSize;
+        } else if (([
           V8SnapshotNodeTypes.string,
-        V8SnapshotNodeTypes.concatenated_string,
-        V8SnapshotNodeTypes.sliced_string
-      ]).indexOf(node.type as V8SnapshotNodeTypes) > -1) {
-        size.string += nodeSize;
-      } else if (node.name === 'Array') {
-        size.array += this.calculateArraySize(node);
-      } else {
-        size.others += nodeSize;
-      }
-    });
-    return size;
+          V8SnapshotNodeTypes.concatenated_string,
+          V8SnapshotNodeTypes.sliced_string
+        ]).indexOf(node.type as V8SnapshotNodeTypes) > -1) {
+          this.calculateSize.string += nodeSize;
+        } else if (node.name === 'Array') {
+          this.calculateSize.array += this.calculateArraySize(node);
+        } else {
+          this.calculateSize.others += nodeSize;
+        }
+      });
+    }
+    return this.calculateSize;
   };
 
   // 计算数组大小
@@ -79,6 +92,17 @@ export class V8Snapshot {
     });
     return size;
   };
+
+  // 获取子节点
+  public getChildren = (nodeId?: number): { edge: V8SnapshotInfoEdge, node: V8SnapshotInfoNode }[] => {
+    let node_id = nodeId === undefined ? this.snapshot_info.root_id : nodeId;
+    return this.snapshot_info.edges[node_id].map(edge=>{
+      return {
+        edge,
+        node: this.snapshot_info.nodes[edge.to_node]
+      }
+    }) || [];
+  }
 }
 
 export default V8Snapshot;
--- vuepress/.vuepress/components/V8Snapshot/V8SnapshotContainment.vue
+++ vuepress/.vuepress/components/V8Snapshot/V8SnapshotContainment.vue
@@ -10,7 +10,22 @@
         <div v-else>未选择</div>
       </div>
       <div>
-        待实现
+        <div v-if="!activeSnapshot">未选择</div>
+        <div v-else-if="!activeSnapshot.snapshot" v-loading="true">解析中...</div>
+        <el-table
+            v-else
+            :data="rootList"
+            row-key="rowKey"
+            style="width: 100%"
+            size="small"
+            lazy
+            :load="load"
+            :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+          <el-table-column fixed prop="name" sortable label="对象"/>
+          <el-table-column prop="distance" sortable label="距离"/>
+          <el-table-column prop="self_size" sortable label="浅层大小"/>
+          <el-table-column prop="retained_size" sortable label="保留大小"/>
+        </el-table>
       </div>
     </el-card>
   </div>
@@ -23,8 +38,36 @@ const { mapGetters } = createNamespacedHelpers('V8Snapshot');
 export default {
   name: "V8SnapshotContainment",
   computed: {
+    rootList(){
+      const result = this.getNodeChildren();
+      return result;
+    },
     ...mapGetters(["activeSnapshot"])
   },
+  methods: {
+    load(tree, treeNode, resolve) {
+      if(!this.activeSnapshot || !this.activeSnapshot.snapshot){
+        resolve([]);
+        return;
+      }
+      const result = this.getNodeChildren(tree.id );
+      resolve(result);
+    },
+    getNodeChildren(nodeId){
+      return this.activeSnapshot.snapshot.getChildren(nodeId).map(item => {
+        const edges = this.activeSnapshot.snapshot.snapshot_info.edges[item.node.id];
+        return {
+          ...item.node,
+          ...item.edge,
+          rowKey: `${item.edge.name_or_index}_${item.edge.to_node}`,
+          hasChildren: !!(edges && edges.length)
+        }
+      });
+    }
+  },
+  mounted() {
+
+  }
 }
 </script>
 
